33e1a103d17c32531fe360380e0e02d4d6ba82db
Daniel Alejandro Gutierrez <dagmcisco@gmail.com>
2018-10-11 16:44:14 -0400

    mi primer commit

---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import 'rxjs/add/operator/map';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor( public afAuth: AngularFireAuth) { }
+
+
+  logOut(){
+    return this.afAuth.auth.signOut();
+  }
+
+  registerUser(email:string, pass: string){
+    return new Promise((resolve, reject)=>{
+      this.afAuth.auth.createUserWithEmailAndPassword(email,pass).then(userData => resolve(userData),err => reject(err));
+    });
+  }
+
+  loginUser(email:string, pass: string){
+    return new Promise((resolve, reject)=>{
+      this.afAuth.auth.signInWithEmailAndPassword(email,pass).then(userData => resolve(userData),err => reject(err));
+    });
+  }
+
+  getAuth(){
+    return this.afAuth.authState.map( auth => auth);
+  }
+    
+
+
+
+}
